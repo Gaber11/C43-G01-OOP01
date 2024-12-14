bcff0f1e12dc79a03d9b8213000ea864090013b9
Gaber11 <gaberezzat0@gmail.com>
2024-12-14 20:42:04 +0200

    2. Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.

--- Assignmet/Program.cs
+++ Assignmet/Program.cs
@@ -14,6 +14,10 @@
         //    sunday
         //}
         #endregion
+
+        #region
+
+        #endregion
         static void Main(string[] args)
         {
 
@@ -28,7 +32,20 @@
 
             #endregion
 
-            #region
+            #region 2.Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+
+            //Person[] person = new Person[4];
+            //person[0] = new Person("Gaber", 24);
+            //person[1] = new Person("Ahmed", 22);
+            //person[2] = new Person("Hassan", 23);
+            //person[3] = new Person("Abdo", 19);
+
+
+            //for (int i = 0; i < person.Length; i++)
+            //{
+            //    Console.WriteLine($"Name: {person[i].Name}, Age: {person[i].Age}");
+            //}
+
 
             #endregion
 
